c855dfee37d1c514ece1ee177dc700746a4789be
Kathure <lizkathure@gmail.com>
2016-06-06 19:43:43 +0300

    added video to main activity

--- app/src/main/java/com/kathure/minions/MainActivity.java
+++ app/src/main/java/com/kathure/minions/MainActivity.java
@@ -2,6 +2,10 @@ package com.kathure.minions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final VideoView myvideoView = (VideoView) findViewById(R.id.videoView);
+
+        myvideoView.setVideoPath("https://static.videezy.com/system/resources/previews/000/000/160/original/RoadTrip.mp4");
+
+        //play pause stop
+
+        MediaController mediaController = new MediaController (this);
+        mediaController.setAnchorView(myvideoView);
+        myvideoView.setMediaController(mediaController);
+
+
+        myvideoView.start();
+
     }
+
 }
--- app/src/main/java/com/kathure/minions/MinionsIntro.java
+++ app/src/main/java/com/kathure/minions/MinionsIntro.java
@@ -1,5 +1,6 @@
 package com.kathure.minions;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
@@ -23,12 +24,12 @@ public class MinionsIntro extends AppIntro {
             setSeparatorColor(Color.parseColor("#2196F3"));
 
             // Hide Skip/Done button.
-            showSkipButton(false);
-            setProgressButtonEnabled(false);
+            showSkipButton(true);
+            setProgressButtonEnabled(true);
 
             // Turn vibration on and set intensity.
             // NOTE: you will probably need to ask VIBRATE permisssion in Manifest.
-            setVibrate(true);
+            setVibrate(false);
             setVibrateIntensity(30);
         }
 
@@ -36,13 +37,17 @@ public class MinionsIntro extends AppIntro {
         @Override
         public void onSkipPressed(Fragment currentFragment) {
             super.onSkipPressed(currentFragment);
-            // Do something when users tap on Skip button.
+
+            Intent intent = new Intent(MinionsIntro.this,MainActivity.class);
+            startActivity(intent);
         }
 
         @Override
         public void onDonePressed(Fragment currentFragment) {
             super.onDonePressed(currentFragment);
-            // Do something when users tap on Done button.
+
+            Intent intent =new Intent(MinionsIntro.this,MainActivity.class);
+            startActivity(intent);
         }
 
         @Override
